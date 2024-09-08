5bbb630d31c903591c3ff24e8869f48330a144c9
kpetow <92036249+kpetow@users.noreply.github.com>
2024-09-07 23:57:46 -0500

    vite png optimization

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import viteImagemin from 'vite-plugin-imagemin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteImagemin({
+      optipng: {
+        optimizationLevel: 7
+      },
+      pngquant: {
+        quality: [0.65, 0.8]
+      },
+      mozjpeg: {
+        quality: 80
+      },
+    })
+  ],
 })
